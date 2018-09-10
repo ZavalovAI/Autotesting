ec95b620117a0026cbed6b8d3a996fb0068594f7
ZavalovAI <Andrey.Zavalov@gmail.com>
2018-09-10 19:18:50 +0300

    Initial commit

---
+++ src/FirstProgram.java
@@ -0,0 +1,8 @@
+public class FirstProgram {
+    public static void main(String[] args){
+        String out = "Hello World!";
+        int i = 127;
+        System.out.println(out + " " + i);
+
+    }
+}
